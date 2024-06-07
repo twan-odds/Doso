685a46553547b7980b98aeed3eec18d60ab71d33
Pongsaphak <39190909+pongsaphakmw@users.noreply.github.com>
2024-06-07 14:47:04 +0700

    remove unused var

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,52 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 import './App.css'
-import { NewTodoForm } from './NewTodoForm'
-import { TodoList } from './TodoList';
+// import { NewTodoForm } from './NewTodoForm'
+// import { TodoList } from './TodoList';
 import { Navbar } from './Navbar';
 import { Home } from './Home';
 
-interface Todo {
-  id: string;
-  title: string;
-  completed: boolean;
-}
+// interface Todo {
+//   id: string;
+//   title: string;
+//   completed: boolean;
+// }
 
 function App() {
 
-  const [todos, setTodos] = useState<Todo[]>(() => {
-    const localValue = localStorage.getItem('TODOS');
-    return localValue ? JSON.parse(localValue) : [];
-  });
+  // const [todos, setTodos] = useState<Todo[]>(() => {
+  //   const localValue = localStorage.getItem('TODOS');
+  //   return localValue ? JSON.parse(localValue) : [];
+  // });
 
-  const handleSubmit = (value: string) => {
-    setTodos([
-      ...todos,
-      {
-        id: crypto.randomUUID(),
-        title: value,
-        completed: false,
-      },
-    ]);
-  };
+  // const handleSubmit = (value: string) => {
+  //   setTodos([
+  //     ...todos,
+  //     {
+  //       id: crypto.randomUUID(),
+  //       title: value,
+  //       completed: false,
+  //     },
+  //   ]);
+  // };
   
-  const handleCheck = (id: string, completed: boolean) => {
-    setTodos((currentTodos) => {
-      return currentTodos.map((todo) => {
-        if (todo.id === id) {
-          return { ...todo, completed };
-        }
-        return todo;
-      });
-    });
-  };
+  // const handleCheck = (id: string, completed: boolean) => {
+  //   setTodos((currentTodos) => {
+  //     return currentTodos.map((todo) => {
+  //       if (todo.id === id) {
+  //         return { ...todo, completed };
+  //       }
+  //       return todo;
+  //     });
+  //   });
+  // };
   return (
     <>
     <Navbar/>
-      <NewTodoForm onSubmit={handleSubmit}/>
+      {/* <NewTodoForm onSubmit={handleSubmit}/>
       <TodoList
         todos={todos}
         onCheckTodo={handleCheck}
-      />
+      /> */}
       <Home/>
     </>
   )
